7370f99ac164c02e9dcba1fb8b22a309152c2e03
Aaron Campbell <aaron@keydesign.com>
2024-02-03 16:17:15 +0200

    Added FastAPI setup

---
+++ application-api/main.py
@@ -0,0 +1,12 @@
+from fastapi import fastapi
+
+# Setup FastAPI application
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Hello world."
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=7777)
