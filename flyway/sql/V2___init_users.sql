452a7164d22a42e4397b61a317c1057f32ddf1d0
Aaron Campbell <aaron@keydesign.com>
2024-02-28 15:39:45 +0200

    Added more dummy table info

--- flyway/sql/V2___init_users.sql
+++ flyway/sql/V2___init_users.sql
@@ -1,3 +1,4 @@
+-- Create dummy users
 INSERT INTO users (name, password, friendly_name) VALUES ('user1','pass1','User 1');
 INSERT INTO users (name, password, friendly_name) VALUES ('user2','pass2','User 2');
 INSERT INTO users (name, password, friendly_name) VALUES ('user3','pass3','User 3');
---
+++ flyway/sql/V4___init_messages.sql
@@ -0,0 +1,19 @@
+-- Room: user1:user2
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user1', 1, 'Hello user2! How are you today?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user2', 1, 'Hi user1! I''m doing well, thanks. Any plans for the day?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user1', 1, 'Thinking about lunch. Any preferences?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user2', 1, 'How about grabbing some pizza?');
+
+-- Room: user3:user4
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user3', 2, 'Hey user4! What''s up?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user4', 2, 'Not much, just chilling. Any exciting news?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user3', 2, 'I was thinking of watching a movie this weekend. Any suggestions?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user4', 2, 'How about checking out the new action movie at the theater?');
+
+-- Room: Aalto Internet Protocols
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user1', 3, 'Hello team! Excited about our upcoming chat-app project!');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user2', 3, 'Absolutely! Let''s discuss our roles and responsibilities.');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user3', 3, 'I''ve been brainstorming some ideas for the user interface. Any preferences?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user4', 3, 'I''m focusing on the backend.');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user2', 3, 'Great! Are there any specific functionalities we should prioritize?');
+INSERT INTO messages (sender_name, room_id, message) VALUES ('user1', 3, 'I think real time communication would be a nice goal.');
