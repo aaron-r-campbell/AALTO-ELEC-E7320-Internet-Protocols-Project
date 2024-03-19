8f469c432c865d01757d5beca850f7ebe8e57715
Aaron Campbell <aaron@keydesign.com>
2024-03-19 03:45:38 +0200

    Fixed UTC again

--- application-api/utils/auth_util.py
+++ application-api/utils/auth_util.py
@@ -1,6 +1,6 @@
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from fastapi import Depends
 from dotenv import load_dotenv
 import os
@@ -18,7 +18,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def create_jwt_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.now(datetime.UTC) + (
+    expire = datetime.now(UTC) + (
         expires_delta
         if expires_delta is not None
         else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
