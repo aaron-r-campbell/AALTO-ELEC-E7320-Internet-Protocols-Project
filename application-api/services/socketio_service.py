3ad19a98e5ccc05c2ddae0a0192ad29d2bad9bca
JoonasBjork <joonas.bjork@gmail.com>
2024-03-20 22:06:10 +0200

    Fix bug

--- application-api/services/socketio_service.py
+++ application-api/services/socketio_service.py
@@ -501,7 +501,7 @@ async def join_file_edit(sid, file_id):
 async def update_document(sid, file_id, operation_type, char, position):
     try:
         print(f"update_document has been called to {file_id}")
-        print(files[file_id])
+        print(file_content_list[file_id])
         file_id = int(file_id)
 
         # Get the user name from the session
