74586fb31447e0cf9dd6d21a62b309c1e1be3098
JoonasBjork <joonas.bjork@gmail.com>
2024-03-20 21:53:39 +0200

    Add base for file download

---
+++ application-ui/src/components/files/FileDownload.svelte
@@ -0,0 +1,15 @@
+<!-- <script>
+</script>
+
+{#if !hidden}
+  <div>
+    <button
+      type="submit"
+      on:click={() => {
+        hidden = true;
+        console.log("Submit pressed");
+        handleButton();
+      }}>Submit</button
+    >
+  </div>
+{/if} -->
