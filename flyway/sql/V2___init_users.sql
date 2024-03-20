a043efdfa05736c7e2a889de09a5f9d343f3b9d7
Aaron Campbell <aaron@keydesign.com>
2024-03-20 19:49:46 +0000

    More default users

--- flyway/sql/V2___init_users.sql
+++ flyway/sql/V2___init_users.sql
@@ -4,3 +4,8 @@ INSERT INTO users (username, password) VALUES ('user2','pass2');
 INSERT INTO users (username, password) VALUES ('user3','pass3');
 INSERT INTO users (username, password) VALUES ('user4','pass4');
 INSERT INTO users (username, password) VALUES ('user5','pass5');
+INSERT INTO users (username, password) VALUES ('user6','pass6');
+INSERT INTO users (username, password) VALUES ('user7','pass7');
+INSERT INTO users (username, password) VALUES ('user8','pass8');
+INSERT INTO users (username, password) VALUES ('user9','pass9');
+INSERT INTO users (username, password) VALUES ('user10','pass10');
